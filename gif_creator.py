0bb039c174ebe6d4af4076b9ff4ccd2d5c675a44
gabrielagustin <gabiagus@gmail.com>
2019-03-20 08:38:37 -0300

    add changes

--- gif_creator.py
+++ gif_creator.py
@@ -20,22 +20,12 @@ arr = os.listdir(pathMap)
 arr.sort()
 print(arr)
 
-# print( filter(lambda x: x.endswith('.png'), os.listdir(pathMap)))
-# print(arr2)
+with imageio.get_writer(pathMap+str('/Map.gif'), mode='I', duration=0.9) as writer:
+    for i in arr:
+        if (i.endswith('.png')):
+            print(i)
+            image = imageio.imread(pathMap+(i))
+            writer.append_data(image)
 
 
-# with imageio.get_writer(pathMap+str('/Mapas.gif'), mode='I', duration=0.9) as writer:
-#     for i in arr:
-#         if ()
-#         print(i)
-#         path2 = path + i
-# #        fileVV = path2 +"/Sigma0_VV_db.img"
-# #        fileVV = i
-#         src_ds_VV, bandVV, GeoTVV, ProjectVV = functions.openFileHDF(fileVV, 1)
-# #        im = plt.imshow(bandVV)
-# #        +"Histograma_"
-# #        image = imageio.imread(pathHistogramas+(i))
-#         image = imageio.imread(pathMapasInu+(i))
-#         writer.append_data(image)
-    
 
