332ca37b4d09451ac0275aee90882e31fee0db1e
gabrielagustin <gabiagus@gmail.com>
2019-04-08 10:36:35 -0300

    add changes

--- histogram_evolution_plot.py
+++ histogram_evolution_plot.py
@@ -7,41 +7,56 @@ Created on Thu Jun 28 14:46:41 2018
 
 Script that allows to obtain the graph 
 of the temporal evolution of the histogram of the SAR images.
+In gray the histogram of the initial state appears and superimposed
+in green appears the other states.
 
 """
 
+import os
+from matplotlib import pyplot as plt
+import functions
 
-# # ###### codigo para mostrar los histogramas superpuestos
-# # #fig, ax = plt.subplots()
-# # #
-# # #for i in arr:
-# # #    print(i)
-# # #    ax.cla()
-# # #    fileVV = path + "20150109.data" +"/Sigma0_VV_db.img"
-# # #    src_ds_VV_0, bandVV_0, GeoTVV_0, ProjectVV_0 = functions.openFileHDF(fileVV, 1)
-# # #    transform = GeoTVV_0
-# # #    src_ds_VV = src_ds_VV_0
-# # #    xmin,xmax,ymin,ymax=transform[0],transform[0]+transform[1]*src_ds_VV.RasterXSize,transform[3]+transform[5]*src_ds_VV.RasterYSize,transform[3]
-# # #    lcNew_0 = bandVV_0.flatten('C')
-# # #    y, x, _= plt.hist(lcNew_0, 500, [-30,-1], facecolor='gray', normed=1) 
-# # #    
-# # #    #fecha = i[:-5]
-# # #    #plt.title(fecha)
-# # #    plt.xlim(-30, -1)
-# # #    plt.ylim(0, 0.3)
-# # #
-# # #    if (i != "20150109.data"):
-# # #        path2 = path + i
-# # #        fileVV = path2 +"/Sigma0_VV_db.img"
-# # #        src_ds_VV, bandVV, GeoTVV, ProjectVV = functions.openFileHDF(fileVV, 1)
-# # #        transform = GeoTVV
-# # #        src_ds_VV = src_ds_VV
-# # #        xmin,xmax,ymin,ymax=transform[0],transform[0]+transform[1]*src_ds_VV.RasterXSize,transform[3]+transform[5]*src_ds_VV.RasterYSize,transform[3]
-# # #    
-# # #        lcNew = bandVV.flatten('C')
-# # #        y_i, x_i, _= plt.hist(lcNew, 500, [-30,-1], normed=1, facecolor='g', alpha=0.75) 
-# # # 
-# # #        fecha = i[:-5]
-# # #        plt.savefig(str("/home/stanza/Downloads/nuevasInunadaciones/Recorte_dB/") + str(fecha) +'.png')
-# # #        plt.show()
+
+path = "/media/gag/Datos/Estancia_Italia_2018/Sentinel-1A-SantaFe/Recorte_dB/"
+arr = os.listdir(path)
+### ordeno la lista
+arr.sort()
+print(arr)
+
+
+##### codigo para mostrar los histogramas superpuestos
+fig, ax = plt.subplots()
+
+for i in arr:
+   print(i)
+   ax.cla()
+   ### initial state
+   fileVV = path + "20150109.data" +"/Sigma0_VV_db.img"
+   src_ds_VV_0, bandVV_0, GeoTVV_0, ProjectVV_0 = functions.openFileHDF(fileVV, 1)
+   transform = GeoTVV_0
+   src_ds_VV = src_ds_VV_0
+   xmin,xmax,ymin,ymax=transform[0],transform[0]+transform[1]*src_ds_VV.RasterXSize,transform[3]+transform[5]*src_ds_VV.RasterYSize,transform[3]
+   lcNew_0 = bandVV_0.flatten('C')
+   y, x, _= plt.hist(lcNew_0, 500, [-30,-1], facecolor='gray', normed=1) 
+   
+   #fecha = i[:-5]
+   #plt.title(fecha)
+   plt.xlim(-30, -1)
+   plt.ylim(0, 0.3)
+
+   if (i != "20150109.data"):
+       ### for other states
+       path2 = path + i
+       fileVV = path2 +"/Sigma0_VV_db.img"
+       src_ds_VV, bandVV, GeoTVV, ProjectVV = functions.openFileHDF(fileVV, 1)
+       transform = GeoTVV
+       src_ds_VV = src_ds_VV
+       xmin,xmax,ymin,ymax=transform[0],transform[0]+transform[1]*src_ds_VV.RasterXSize,transform[3]+transform[5]*src_ds_VV.RasterYSize,transform[3]
+   
+       lcNew = bandVV.flatten('C')
+       y_i, x_i, _= plt.hist(lcNew, 500, [-30,-1], normed=1, facecolor='g', alpha=0.75) 
+
+       fecha = i[:-5]
+    #    plt.savefig(str("/home/stanza/Downloads/nuevasInunadaciones/Recorte_dB/") + str(fecha) +'.png')
+       plt.show()
 
