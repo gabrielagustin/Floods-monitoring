dec73f0e8ca3755303aad580c85f82bf1b1cc689
gabrielagustin <gabiagus@gmail.com>
2019-07-08 12:36:15 -0300

    create a unique function to create a .gif using different types of sources images

--- gif_creator.py
+++ gif_creator.py
@@ -6,7 +6,7 @@ Created on Tue Feb  5 10:58:49 2019
 
 @author: gag
 
-Script que genera una imagen .gif a partir de una compilación de imagenes .png o .img 
+Script that generate an image .gif from a compilation images .png or .img
  
 """
 
@@ -16,11 +16,17 @@ import imageio
 
 
 def create_gif_from_png(pathIn, pathOut):
-    """ 
-    Generate .gif file using a .png files compilation
-        pathIn: directory where are .png files
-        pathOut: directory where .gif file is created
+    """ Generate .gif file using a .png files compilation
+
+    Parameters:
+    -----------
+    pathIn : complete path of the image to read
+    pathOut : complete path of the image to be created
+
+    Returns: 
+    --------
     """
+
     arr = os.listdir(pathIn)
     arr.sort()
     print(arr)
@@ -34,31 +40,39 @@ def create_gif_from_png(pathIn, pathOut):
     return
 
 
-def create_gif_from_img(pathIn, pathOut):
-    """ 
-    Generate .gif file using a .img files compilation
-        pathIn: directory where are .png files
-        pathOut: directory where .gif file is created
-    """
-    #pathHistogramas = "/home/stanza/Downloads/nuevasInunadaciones/Recorte_dB/"
-    pathMapasInu = "/home/stanza/Downloads/nuevasInunadaciones/Mapas/"
-    with imageio.get_writer('/home/stanza/Mapas.gif', mode='I', duration=0.9) as writer:
-        for i in arr:
-            if (i.endswith('.png')):
-                print(i)
-            path2 = path + i
-    #        fileVV = path2 +"/Sigma0_VV_db.img"
-    #        fileVV = i
-            src_ds_VV, bandVV, GeoTVV, ProjectVV = functions.openFileHDF(fileVV, 1)
-    #        im = plt.imshow(bandVV)
-    #        +"Histograma_"
-    #        image = imageio.imread(pathHistogramas+(i))
-            image = imageio.imread(pathMapasInu+(i))
-            writer.append_data(image)
-        
-    #    
-#    
 
+def create_gif(pathIn, extImg, nameFileOut):
+    """ Generate .gif file using a .img files compilation
+
+    Parameters:
+    -----------
+    pathIn : complete path of drthe image to read
+    extImg : type of image to read ".PNG", ".IMG"
+    nameFileOut : name of the image to be created
+
+    Returns: 
+    --------
+    """
+
+    arr = os.listdir(pathIn)
+    arr.sort()
+
+    with imageio.get_writer(pathIn + '/' + nameFileOut, mode='I', duration=0.9) as writer:
+        for i in arr:
+            if(extImg == '.png'):                   
+                if (i.endswith('.png')):
+                    print(i)
+                    image = imageio.imread(pathIn+(i))
+                    writer.append_data(image)     
+            if(extImg == '.img'):                   
+                if (i.endswith('.img')):
+                    print(i)
+                    image = imageio.imread(pathIn+(i))
+                    writer.append_data(image)     
+    ## deberia saber si se modifico o si realmente se modifico el archivo de salida
+    print("Archivo creado con exito!!!")
+    return
+                
 
 
 if __name__ == "__main__":
